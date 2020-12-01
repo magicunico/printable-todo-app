bfeaf224fcc1d06b605eb2e25977eca388c8d4cc
Jula13 <jula7092@gmail.com>
2020-12-01 23:28:51 +0100

    added different styles files

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
 
-const pdfTemplate = require('./documents')
+const halloweenPdfTemplate = require('./documents')
+const christmasPdfTemplate = require('./christmas')
+const darkPdfTemplate = require('./dark')
+const darkPdfTemplate = require('./light')
 
 const app = express();
 
@@ -15,8 +18,36 @@ app.use(bodyParser.json());
 
 //POST generate PDF
 
-app.post('/create-pdf',(req,res)=>{
-    pdf.create(pdfTemplate(req.body),{}).toFile('MyToDo.pdf',(err)=>{
+app.post('/create-halloween-pdf',(req,res)=>{
+    pdf.create(halloweenPdfTemplate(req.body),{}).toFile('haloweenPdf.pdf',(err)=>{
+        if(err){
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });  
+})
+
+
+app.post('/create-christmas-pdf',(req,res)=>{
+    pdf.create(christmasPdfTemplate(req.body),{}).toFile('christmasPdf.pdf',(err)=>{
+        if(err){
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });  
+})
+
+app.post('/create-light-pdf',(req,res)=>{
+    pdf.create(lightPdfTemplate(req.body),{}).toFile('lightPdf.pdf',(err)=>{
+        if(err){
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });  
+})
+
+app.post('/create-dark-pdf',(req,res)=>{
+    pdf.create(darkPdfTemplate(req.body),{}).toFile('darkPdf.pdf',(err)=>{
         if(err){
             res.send(Promise.reject());
         }
@@ -26,10 +57,22 @@ app.post('/create-pdf',(req,res)=>{
 
 
 //GET send pdf 
-app.get('/fetch-pdf',(req,res)=>{
-    res.sendFile(`${__dirname}/MyToDo.pdf`)
+app.get('/fetch-halloween-pdf',(req,res)=>{
+    res.sendFile(`${__dirname}/haloweenPdf.pdf`)
+})
+
+app.get('/fetch-christmas-pdf',(req,res)=>{
+    res.sendFile(`${__dirname}/christmasPdf.pdf`)
+})
+
+app.get('/fetch-light-pdf',(req,res)=>{
+    res.sendFile(`${__dirname}/lightPdf.pdf`)
+})
+
+app.get('/fetch-dark-pdf',(req,res)=>{
+    res.sendFile(`${__dirname}/darkPdf.pdf`)
 })
 
 
-app.listen(port, ()=>console.log("Listening on port `${task10}"));
+app.listen(port, ()=>console.log(`Listening on port ${task10}`));
 
